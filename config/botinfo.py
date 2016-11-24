653177fe80a89549f2d440e81d630332ff7dfda3
ZweiSpeedruns <zcm727@gmail.com>
2016-11-24 12:25:14 -0600

    Added config example by fixing work structure
    The config file is to go in the root directory. I always had this
    example file in the repo, but an error involving the gitignore file
    caused it to be ignored alongside the actual botinfo.py.
    
    Signed-off-by: ZweiSpeedruns <zcm727@gmail.com>

---
+++ config/botinfo.py
@@ -0,0 +1,8 @@
+token = "Bot token here"
+serverid = "Server id here"
+operatorRole = "Name of operator role here"
+roleList = ["Names",
+            "of",
+            "available",
+            "roles",
+            "here"] # I may do some changing here in the future
