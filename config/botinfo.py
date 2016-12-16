edc77b79f41e17fa48a68f6f9d461a7209d811a0
ZweiSpeedruns <zcm727@gmail.com>
2016-12-16 10:22:20 -0600

    Modified role list format and added makefile
    Signed-off-by: ZweiSpeedruns <zcm727@gmail.com>

--- config/botinfo.py
+++ config/botinfo.py
@@ -1,8 +1,6 @@
-token = "Bot token here"
-serverid = "Server id here"
-operatorRole = "Name of operator role here"
-roleList = ["Names",
-            "of",
-            "available",
-            "roles",
-            "here"] # I may do some changing here in the future
+roleListFile = open("roleList.txt", 'r') # Open role list file
+roleList = roleListFile.read().splitlines() # Get role list from file
+
+token = "MjA5Nzk4NDQ5ODI1MTg1Nzkz.CxMKOQ.3s5PJAL_kQOJKbHagXQFIgmDY5Y" # bot token
+serverid = "249837317362155520" # server bot is to operate in
+operatorRole = "Bot Commander" # role name for operator
